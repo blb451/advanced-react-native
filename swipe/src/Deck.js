28e136e0556ac0932f343d0a916fe3c8b1cbb79c
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-05-12 12:34:38 -0700

    2.14

---
+++ swipe/src/Deck.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View, Animated } from 'react-native';
+
+class Deck extends Component {
+  render() {
+    return (
+      <View></View>
+    );
+  }
+}
+export default Deck;
