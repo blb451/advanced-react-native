b60aad9d13426c6309f7064ab828466fe91106a3
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-05-12 13:12:30 -0700

    3.18

--- swipe/main.js
+++ swipe/main.js
@@ -1,6 +1,7 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 
 import Deck from './src/Deck';
 
@@ -18,7 +19,20 @@ const DATA = [
 class App extends React.Component {
   renderCard(item) {
     return (
-      <Text>{item.text}</Text>
+      <Card
+        key={item.id}
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
+        <Text style={{ marginBottom: 10 }}>
+          I can customize the Card further.
+        </Text>
+        <Button 
+          icon={{ name: 'code' }}
+          backgroundColor="#03A9F4"
+          title="View Now!"
+        />
+      </Card>
     );
   }
 
--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const panResponder = PanResponder.create({
+
+    });
+
+    // Probably not the best idea to assign this to state, but for the sake of following docs:
+    this.state = { panResponder };
+  }
+
+  renderCards() {
+    return this.props.data.map(item => {
+      return this.props.renderCard(item);
+    });
+  }
+
   render() {
     return (
-      <View></View>
+      <View>
+        {this.renderCards()}
+      </View>
     );
   }
 }
